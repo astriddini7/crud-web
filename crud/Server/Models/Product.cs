0ca9306a6b2aa4ab8f9511a7317f6f3681778399
astrid <astridmagang@gmail.com>
2023-06-05 23:28:34 +0700

    🔨 create model

---
+++ crud/Server/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace crud.Server.Models
+{
+    public class Product
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString("N");
+        public string Name { get; set; } = default!;
+        public string Description { get; set; } = default!;
+    }
+}
